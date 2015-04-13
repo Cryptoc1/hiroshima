82b6712db437016286c70b3a7610f157abf89e7a
Samuel Steele <steelesam72@gmail.com>
2015-04-13 01:41:55 -0400

    Made install sript into a bash script instead of python.

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "This install script will take (a very few) steps to setting up Hiroshima to work properly."
+echo "You will be asked to enter your password multiple times throughout the process, so don't go anywhere (not that this is a long process anyway)."
+
+if [ -d "/Library/WebServer/Documents/hiroshima" ]; then
+	echo "Deleting old files..."
+	sudo rm -rf /Library/WebServer/Documents/hiroshima
+fi
+
+echo "Moving files into the WebServer..."
+sudo cp -R hiroshima-web-files /Library/WebServer/Documents/hiroshima
+
+echo "Starting Apache (restarting if already enabled)..."
+sudo apachectl restart
+
+echo "Process complete, wasn't so bad was it? Follow the instructions in installation-instructions.md"
