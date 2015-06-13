326db5a7c0a112244273ee1e54977c6c0792070b
Samuel Steele <steelesam72@gmail.com>
2015-06-13 16:34:43 -0400

    Cleaned up dependencies installl

--- install.sh
+++ install.sh
@@ -16,35 +16,10 @@ sudo apachectl restart
 
 echo "Installing dependencies..."
 
-echo "Installing python-twitter..."
-pip install python-twitter --upgrade
-if [ -d "./python-twitter" ]; then
-	echo "Deleting old clone..."
-	rm -rf ./twitter
-fi
-echo "Cloning python-twitter..."
-git clone http://github.com/bear/python-twitter.git
-if [ -d "./twitter" ]; then
-	echo "Deleting old copy of python-twitter..."
-	rm -rf ./twitter
-fi
-echo "Moving python-twitter/twitter to ./twitter..."
-mv python-twitter/twitter ./
+echo "Installing tweepy..."
+pip install tweepy --upgrade
 
 echo "Installing python-instagram..."
 pip install python-instagram --upgrade
-if [ -d "./python-instagram" ]; then
-	echo "Deleting old clone..."
-	rm -rf ./python-instagram
-fi
-echo "Cloning python-instagram..."
-git clone https://github.com/instagram/python-instagram.git
-if [ -d "./instagram" ]; then
-	echo "Deleting old copy of python-instagram..."
-	rm -rf ./instagram
-fi
-echo "Moving python-instagram/instagram to ./instagram..."
-mv python-instagram/instagram ./instagram
-
 
 echo "Process complete, wasn't so bad was it? Follow the instructions in installation-instructions.md"
