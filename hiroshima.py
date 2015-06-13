83825c18ca4f6eba0983740d245747e8c48a0609
Samuel Steele <steelesam72@gmail.com>
2015-06-12 21:09:53 -0400

    Begin major refactoring

---
+++ hiroshima.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python
+
+##
+# 
+# hiroshima.py
+# This is the main front-end that pulls all network attacks into one interface. Enjoy.
+# Samuel Steele (cryptoc1)
+#
+##
+
+import networks
+import os, sys
+
+DEVMODE = True
+
+_usage = "hiroshima help: \
+            \n\tAttackable Networks: \
+                \n\t\t+ Twitter \
+                \n\t\t+ Instagram \
+                \n\t\t+ AskFM \
+            \n\tAttack Types: \
+                \n\t\tTwitter: \
+                    \n\t\t\t+ Favorite \
+                    \n\t\t\t+ Reply \
+                    \n\t\t\t+ Un-favorite \
+                \n\t\tInstagram: \
+                    \n\t\t\t+ Like \
+                    \n\t\t\t+ Un-like \
+                \n\t\tAskFM: \
+                    \n\t\t\t+ Ask Question"
+
+def prologue():
+    print "For help, enter 'help', or '?'. To begin, enter 'begin'."
+    cmd = raw_input("> ").lower()
+    if cmd == "help" or cmd == "?":
+        print _usage
+        prologue()
+    elif cmd == "begin":
+        main()
+    else:
+        prologue()
+
+def main():
+    print "Enter a social network to attack"
+    network = raw_input("> ").lower()
+    if network == "instagram":
+        instagram_attack()
+    elif network == "twitter":
+        twitter_attack()
+    elif network == "askfm":
+        askfm_attack()
+    else:
+        main()
+
+def instagram_attack():
+    insta = networks.Instagram()
+    print "In order for any actions to be preformed, you need to authorize hiroshima to use your Instagram account. Proceed? (Y/n)"
+    prompt = raw_input("> ").lower()
+    if prompt == "y":
+        if insta.login():
+            print "Enter the username of your victim"
+            username = raw_input("> ")
+            print "Searching..."
+            search = insta.search_users(username)
+            print "I found this: "
+            print insta.format_user_info(search)
+            print "Is this the intended victim? (Y/n)"
+            prompt = raw_input("> ").lower()
+            if prompt == "y":
+                if insta.set_victim(search):
+                    print "Enter the attack type (" + insta.get_attack_types() + ")"
+                    attack_type = raw_input("> ").lower()
+                    if attack_type == "like":
+                        print "Enter the number of photos to be liked (enter 'all' to like all)"
+                        insta.like_attack(raw_input("> "))
+                        print "Like attack complete."
+                        prologue()
+                    elif attack_type == "unlike":
+                        print "Enter the number of photos to be liked (enter 'all' to unlike all)"
+                        insta.unlike_attack(raw_input("> "))
+                        print "Unlike attack complete."
+                        prologue()
+                    else:
+                        print "Attack type not entered, starting attack over..."
+                        instagram_attack()
+                else:
+                    print "There was an error setting the victim."
+                    prologue()
+            elif prompt == "n":
+                print "Please check to make sure you have the correct username. (The attack will now start over)."
+                instagram_attack()
+            else:
+                print "Unrecognized input."
+                prologue()
+        else:
+            print "There was an error logining in."
+            prologue()
+
+    elif prompt == "n":
+        print "Okay..."
+        prologue()
+    else:
+        print "Unrecognized character(s)."
+        prologue()
+
+def twitter_attack():
+    pass
+
+def askfm_attack():
+    pass
+
+if __name__ == "__main__":
+    print "Hello, and welcome to Hiroshima: A Social Spammer."
+    prologue()
---
+++ networks.py
@@ -0,0 +1,149 @@
+#!/usr/bin/env python
+
+##
+# 
+# networks.py
+# this file contains the classes for different social network attacks.
+# Samuel Steele (cryptoc1)
+#
+##
+
+
+import sys, os, time, webbrowser
+from instagram.client import InstagramAPI
+
+if len(sys.argv) > 1 and sys.argv[1] == 'local':
+    try:
+        from test_settings import *
+    
+        InstagramAPI.host = test_host
+        InstagramAPI.base_path = test_base_path
+        InstagramAPI.access_token_field = "access_token"
+        InstagramAPI.authorize_url = test_authorize_url
+        InstagramAPI.access_token_url = test_access_token_url
+        InstagramAPI.protocol = test_protocol
+    except Exception:
+        pass
+# Python 2.x
+try:
+    import __builtin__
+    input = getattr(__builtin__, 'raw_input')
+except (ImportError, AttributeError):
+    pass
+    
+class Instagram:
+    def __init__(self):
+               # Stuff for the attack
+        self.delay = 5
+        self.victim = None
+
+        # Stuff for the API
+        self.client_id = 'd00446a61de44643bd0d1a21d78e0f5a'
+        self.client_secret = 'c742af13e3a04138bad288c50e005999'
+        self.access_token = None
+        self.redirect_uri = 'http://localhost/hiroshima/auth/insta'
+        self.raw_scope = 'basic likes'
+        self.scope = self.raw_scope.split(' ')
+        # For basic, API seems to need to be set explicitly
+        if not self.scope or self.scope == [""]:
+            self.scope = ["basic"]
+        self.api = InstagramAPI(client_id=self.client_id, client_secret=self.client_secret, redirect_uri=self.redirect_uri)
+        self.redirect_uri = self.api.get_authorize_login_url(scope=self.scope)
+
+    def login(self):
+        print "You will be redirected to an authorization page in your browser. Copy the code in the prompt and paste it below."
+        webbrowser.open(self.redirect_uri)
+        code = (str(input("code: ").strip()))
+        self.access_token = self.api.exchange_code_for_access_token(code)
+        if self.access_token != None:
+            self.a_api = InstagramAPI(access_token=self.access_token[0])
+            print "You're account has been authorized."
+            return True
+        else:
+            return False
+
+    def set_victim(self, user):
+        self.victim = self.a_api.user(user.id)
+        if self.victim != None:
+            return  True
+        else:
+            return False
+        pass
+
+    def search_users(self, uname):
+        return self.a_api.user(self.a_api.user_search(q=uname, count=1)[0].id)
+
+    def like_attack(self, count):
+        if str(count).lower() == "all":
+            print "Liking " + str(count) + " photos. Due to rate-limits, this may take a while..."
+            print "Number of photos expected to be liked: " + str(self.victim.counts['media'])
+            eta = self.victim.counts['media'] * self.delay
+            print self.format_eta(eta)
+            for media in self.a_api.user_recent_media(user_id=self.victim.id, count=self.victim.counts['media'])[0]:
+                if media.user_has_liked:
+                    print "Photo with id: " + str(media.id) + " already liked, skipping."
+                else:
+                    self.a_api.like_media(media.id)
+                    print "Photo with id: " + str(media.id) + " liked."
+                time.sleep(self.delay)
+        else:
+            count = int(count)
+            print "Liking " + str(count) + " photos. Due to rate-limits, this may take a while..."
+            print "Number of photos expected to be liked: " + str(count)
+            eta = count * self.delay
+            print self.format_eta(eta)
+            for media in self.a_api.user_recent_media(user_id=self.victim.id, count=count)[0]:
+                if media.user_has_liked:
+                    print "Photo with id: " + str(media.id) + " already liked, skipping."
+                else:
+                    self.a_api.like_media(media.id)
+                    print "Photo with id: " + str(media.id) + " liked."
+                time.sleep(self.delay)
+
+    def unlike_attack(self, count):
+        if str(count).lower() == "all":
+            print "Unliking " + str(count) + " photos. Due to rate-limits, this may take a while..."
+            print "Number of photos expected to be unliked: " + str(self.victim.counts['media'])
+            eta = self.victim.counts['media'] * self.delay
+            print self.format_eta(eta)
+            for media in self.a_api.user_recent_media(user_id=self.victim.id, count=self.victim.counts['media'])[0]:
+                if media.user_has_liked:
+                    self.a_api.unlike_media(media.id)
+                    print "Photo with id: " + str(media.id) + " unliked."
+                else:
+                    print "Photo with id: " + str(media.id) + " already not liked, skipping."
+                time.sleep(self.delay)
+        else:
+            count = int(count)
+            print "Unliking " + str(count) + " photos. Due to rate-limits, this may take a while..."
+            print "Number of photos expected to be unliked: " + str(count)
+            eta = count * self.delay
+            print self.format_eta(eta)
+            for media in self.a_api.user_recent_media(user_id=self.victim.id, count=count)[0]:
+                if media.user_has_liked:
+                    self.a_api.unlike_media(media.id)
+                    print "Photo with id: " + str(media.id) + " unliked."
+                else:
+                    print "Photo with id: " + str(media.id) + " already not liked, skipping."
+                time.sleep(self.delay)
+
+
+    def get_attack_types(self):
+        return "like, unlike"
+
+    def format_eta(self, eta):
+        if eta > 60:
+            return "ETA: " + str(eta / 60) + "M"
+        else:
+            return "ETA: " + str(eta) + "S"
+
+    def format_user_info(self, u):
+        return "id: " + u.id + "\nusername: " + u.username + "\nfull_name: " + u.full_name + "\nprofile_picture: " + u.profile_picture + "\nbio: " + u.bio + "\nwebsite: " + u.website + "\ncounts: " + str(u.counts)
+
+class Twitter:
+    def __init__(self, client_secret):
+        pass
+
+class AskFM:
+    def __init__(self):
+        pass
